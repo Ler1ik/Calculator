dec95cee486d3bf1a810d446d843f49dea87475e
Ler1ik <115260617+Ler1ik@users.noreply.github.com>
2022-12-15 02:34:01 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,26 @@
 import tkinter as tk
-
+from tkinter import messagebox
+#
+def add_magic(entry):
+    x = ''
+    s = 0
+    for i in range (len(entry)):
+        if s == 0:
+            if entry[i] in ['+', '-', '×', '÷']:
+                x += entry[i]
+                s += 1
+            else:
+                x += entry[i]
+        else:
+            break
+    return x, s
 
 #Стереть всё
 def Delete_0(Del):
+    calc_etry['state'] = tk.NORMAL
     calc_etry.delete(0, tk.END)
     calc_etry.insert(0, '0')
+    calc_etry['state'] = tk.DISABLED
 
 
 #Стерать последний элемент
@@ -13,45 +29,85 @@ def Delete_1(Del):
     entry = entry[:-1]
     if len(entry) == 0:
         entry = '0'
+    calc_etry['state'] = tk.NORMAL
     calc_etry.delete(0, tk.END)
     calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
 
-#Сложение !!!САША!!!
+#Стерать всё после символа операции
+def Delete_2(Del):
+    entry = calc_etry.get()
+    entry_0, sim = add_magic(entry)
+    if sim == 0:
+        entry = '0'
+    else:
+        entry = entry_0
+    calc_etry['state'] = tk.NORMAL
+    calc_etry.delete(0, tk.END)
+    calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
+
+#Противоположный элемент
+def neg(neg):
+    entry = str(int(calc_etry.get()) * (-1))
+    calc_etry['state'] = tk.NORMAL
+    calc_etry.delete(0, tk.END)
+    calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
+
+#Сложение
 def add_pl(x, y):
-    return(x + y)
+    return str(x + y)
 
-#Вычитание !!!САША!!!
+#Вычитание
 def add_mi(x, y):
-    return(x - y)
+    return str(x - y)
 
-#Умножение !!!САША!!!
+#Умножение
 def add_um(x, y):
-    return(x * y)
+    return str(x * y)
 
-#Деление !!!САША!!!
+#Деление
 def add_de(x, y):
-    return(x / y)
+    try:
+        return str(x / y)
+    except ZeroDivisionError:
+        messagebox.showerror('Ошибка', 'Деление на ноль невозможно')
+        return 0
 
-#Знаменатель "⅟х" !!!САША!!!
+#Знаменатель "⅟х"
 def denominator(denominator):
-    number = float(calc_etry.get()) #Не трогать
-    entry = 1 / number
-    calc_etry.delete(0, tk.END) #Не трогать
-    calc_etry.insert(0, entry) #Не трогать
+    number = float(calc_etry.get())
+    try:
+        entry = str(1 / number)
+        calc_etry['state'] = tk.NORMAL
+        calc_etry.delete(0, tk.END)
+        calc_etry.insert(0, entry)
+        calc_etry['state'] = tk.DISABLED
+    except ZeroDivisionError:
+        messagebox.showerror('Ошибка', 'Деление на ноль невозможно')
+        calc_etry['state'] = tk.NORMAL
+        calc_etry.delete(0, tk.END)
+        calc_etry.insert(0, '0')
+        calc_etry['state'] = tk.DISABLED
 
-#Квадрат "x²" !!!САША!!!
+#Квадрат "x²"
 def sq(square):
-    number = float(calc_etry.get()) #Не трогать
-    entry = number ** 2
-    calc_etry.delete(0, tk.END)  # Не трогать
-    calc_etry.insert(0, entry)  # Не трогать
+    number = float(calc_etry.get())
+    entry = str(number ** 2)
+    calc_etry['state'] = tk.NORMAL
+    calc_etry.delete(0, tk.END)
+    calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
 
-#Квадратный корень "√х" !!!САША!!!
+#Квадратный корень "√х"
 def sq_r(square_root):
-    number = float(calc_etry.get()) #Не трогать
-    entry = number ** 0.5
-    calc_etry.delete(0, tk.END)  # Не трогать
-    calc_etry.insert(0, entry)  # Не трогать
+    number = float(calc_etry.get())
+    entry = str(number ** 0.5)
+    calc_etry['state'] = tk.NORMAL
+    calc_etry.delete(0, tk.END)
+    calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
 
 #Pасчёт_1
 def add_calculation_1(S, x, y):
@@ -88,38 +144,53 @@ def add_calculation_0(entry):
         entry = add_calculation_1(S, x, y)
     else:
         entry = calc_etry.get()
-    return entry
+    return str(entry)
 
 
 # Добавить цифру
 def add_digit(digit):
     entry = calc_etry.get()
-    entry_0 = entry+'0'
-    if entry[0] == '0' and entry_0[1] == '0': #проверка для "."
+    if entry[0] == '0' and len(entry) == 1:
         entry = entry[1:]
+    calc_etry['state'] = tk.NORMAL
     calc_etry.delete(0, tk.END)
     calc_etry.insert(0, entry+digit)
+    calc_etry['state'] = tk.DISABLED
 
 #Добавить знак
 def add_sign(sing):
     entry_0 = calc_etry.get()
-    if entry_0[-1] in ['+', '-', '×', '÷', '.']:
+    if sing in ['.']:
+        K = 0
+        for i in range(len(entry_0)):
+            if entry_0[i] in ['+', '-', '×', '÷'] and (i != len(entry_0) - 1):
+                K = 0
+            elif entry_0[i] in ['.']:
+                K += 1
+        if K > 0:
+            sing = ''
+    if entry_0[-1] in ['+', '-', '×', '÷', '.'] and sing != '':
         entry = entry_0[:-1] + sing
     else:
         entry = entry_0 + sing
     if entry[-1] in ['+', '-', '×', '÷'] and ('+' in entry[:-1] or '-' in entry[:-1] or '×' in entry[:-1] or '÷' in entry[:-1]):
         entry = str(add_calculation_0(entry[:-1])) + entry[-1]
+    calc_etry['state'] = tk.NORMAL
     calc_etry.delete(0, tk.END)
     calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
 
 #Равно
 def equally(eq):
     entry = calc_etry.get()
     if entry[-1] in ['+', '-', '×', '÷']:
         entry = entry + entry[:-1]
-    entry = add_calculation_0(entry)
+    entry = str(add_calculation_0(entry))
+    calc_etry['state'] = tk.NORMAL
     calc_etry.delete(0, tk.END)
     calc_etry.insert(0, entry)
+    calc_etry['state'] = tk.DISABLED
+
 #Меню
 def menu(m):
     menu_top = tk.Toplevel(calc)
@@ -128,6 +199,27 @@ def menu(m):
     menu_top.minsize(width=320, height=500)
     menu_top.maxsize(width=320, height=500)
     tk.Button(menu_top, text='Игра', bd=3, font=('Arial', 13)).grid(row=0, column=0, sticky='wens', padx=2, pady=2)
+
+#Ввод через клавиатуру
+def press_key(event):
+    print(repr(event.char))
+    if event.char.isdigit():
+        add_digit(event.char)
+    if event.char in ['+', '-', '*', '/', '.']:
+        if event.char == '*':
+            event.char = '×'
+        if event.char == '/':
+            event.char = '÷'
+        add_sign(event.char)
+    if event.char in ['=', '\r']:
+        equally(event.char)
+    if event.char == '\x1b':
+        Delete_0(event.char)
+    if event.char == '\x08':
+        Delete_1(event.char)
+    if event.char == '`':
+        Delete_2(event.char)
+
 
 # Главный экран
 calc = tk.Tk()
@@ -138,17 +230,9 @@ calc.maxsize(width=320, height=500)
 # Поле ввода (элементы появляются справа)
 calc_etry = tk.Entry(calc, justify=tk.RIGHT, font=('Arial', 20), width=20)
 calc_etry.insert(0, '0')
+calc_etry['state'] = tk.DISABLED
 calc_etry.grid(row=0, column=0, columnspan=4, sticky='we')
 # Кнопки
-'''
-but0 = ["MC", "MR", "M+", "M-", "MS", "M↓"]
-c = 0
-for i in but0:
-    j0 = str(symbol(i))
-    j1 = 'D_' + j0
-    Button(calc, text=i).grid(row=1, column=c, columnspan=4)
-    c += 1
-'''  # but0
 but1 = [
     "M", "CE", "C", "⌫",
     "⅟х", "x²", "√х", "÷",
@@ -157,6 +241,7 @@ but1 = [
     "1", "2", "3", "+",
     "±", "0", ".", "="
 ]
+calc.bind('<Key>', press_key)
 r, c = 1, 0
 for i in but1:
     if i in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
@@ -175,11 +260,12 @@ for i in but1:
         tk.Button(calc, text=i, bd=3, font=('Arial', 13), command=lambda v=i: Delete_0(v)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
     elif i == '⌫':
         tk.Button(calc, text=i, bd=3, font=('Arial', 13), command=lambda v=i: Delete_1(v)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
+    elif i == 'CE':
+        tk.Button(calc, text=i, bd=3, font=('Arial', 13),  command=lambda v=i: Delete_2(v)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
+    elif i == '±':
+        tk.Button(calc, text=i, bd=3, font=('Arial', 13), command=lambda v=i: neg(v)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
     elif i == 'M':
         tk.Button(calc, text=i, bd=3, font=('Arial', 13), command=lambda v=i: menu(v)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
-    else:
-        tk.Button(calc, text=i, bd=3, font=('Arial', 13)).grid(row=r, column=c, sticky='wens', padx=2, pady=2)
-
     calc.grid_rowconfigure(r, minsize=75)
     calc.grid_columnconfigure(c, minsize=70)
     c += 1
